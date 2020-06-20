05af4a1c01d95ead2336de437baea11ae761a931
Clebson Moura <clebson.sh@gmail.com>
2020-06-19 23:51:35 -0300

    HTML DOM

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+function clicar() {
+  let mensagem = document.getElementById('mensagem')
+  mensagem.style.display = 'block'
+}
+
+function redirecionar() {
+  // window.open('https://google.com')
+  window.location.href = 'https://google.com'
+}
