433aa709e5985ad3cd47aefadc93bfaf438f0c8c
Clebson Moura <clebson.sh@gmail.com>
2020-06-19 22:14:44 -0300

    Aula - Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const nome = 'Clebson Moura'
+let frase = 'Japão é o melhor time do mundo'
+//alert(`Bem vindo ${nome}`)
+console.log(31 + 10)
+console.log(frase.replace('Japão', 'Brasil').toUpperCase())
